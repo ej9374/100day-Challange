9ca656c1fcc505b66266ef7e31397237247db4d6
ej9374 <ej9374@naver.com>
2024-09-26 14:50:55 +0900

    두 수 비교

--- src/_1330.java
+++ src/_1330.java
@@ -1,8 +1,7 @@
-package _1330;
 
 import java.util.Scanner;
 
-public class Main {
+public class _1330 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
